fe858f18c3e7779621ab17b35b60b2b10d160ca9
JordiPalacios <jordipalacioseso@gmail.com>
2024-09-26 20:40:27 +0200

    app modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { Guide } from './components/guide'
+import { Navbar } from "./components"
 
 function App() {
 
   return (
     <>
-      <h1>telefonoaudiolibro.es</h1>
-      <Guide txtSide={'left'} srcImg={'public/img/step1.png'} nameImg={'Paso1'} txtTittle={'¡Descuento!'} txtL1={'Este es el paso 1'}  />
-      <Guide txtSide={'left'} srcImg={'public/img/step1.png'} nameImg={'Paso1'} txtTittle={'¡Descuento!'} txtL1={'Este es el paso 1'} txtL3={'Este es el paso 3'}  />
+      <Navbar />
+      <header>
+        <h1>Lo primero que se olvida de una persona es su voz</h1>
+        <h2>Escucha y guarda para siempre las voces de tus invitados</h2>
+        <button>¡LO NECESITO!</button>
+      </header>      
     </>
   )
 }
