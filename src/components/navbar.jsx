6946e00e664f1d9d39ab2141e567cdc792953b1b
JordiPalacios <jordipalacioseso@gmail.com>
2024-09-26 20:40:09 +0200

    navbar component created

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,36 @@
 import React from 'react'
 
 export const Navbar = () => {
+  const navItems = [
+    {
+      tittle: 'Inicio',
+      label: 'inicio',
+      url: '/#inicio'
+    },
+    {
+      tittle: 'Guia de uso',
+      label: 'guia de uso',
+      url: '/#guia-de-uso'
+    },
+    {
+      tittle: 'Eventos Reales',
+      label: 'eventos reales',
+      url: 'https://www.instagram.com/telefonoaudiolibro.es/',
+      target: "_blank"
+    }
+  ]
+  
   return (
-    <div>navbar</div>
+    <nav>
+      <img src="public/img/audiolibroVS.webp" alt="Telefonoaudiolibro Logo" style={{width: "100px", height: "auto"}} />
+      {navItems.map((link) => (
+        <a 
+        aria-label={link.label}
+        href={link.url}
+        target={link.target}>
+          {link.tittle}
+        </a>
+      ))}
+    </nav>
   )
 }
