31dea6afc6bf60f09129239637dd3666e01a0200
JordiPalacios <jordipalacioseso@gmail.com>
2024-10-07 20:09:50 +0200

    header filled with all information

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import { NavbarMenu } from './navbarMenu'
+import React from "react";
+import { Button } from "./index";
 
 export const Header = () => {
     return (
         <header>
-            <NavbarMenu />
+            <img
+                src="public/img/headerImg.png"
+                alt="Teléfono Audiolibro imagen portada"
+            />
+            <h1>Lo primero que se olvida de una persona es su voz</h1>
+            <h2>Escucha y guarda para siempre las voces de tus invitados</h2>
+            <Button text={"Reservar"} />
         </header>
-    )
-}
+    );
+};
