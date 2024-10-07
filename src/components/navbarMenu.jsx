44620e4a476c2edd37ed6509d9bb2d2159c2334e
JordiPalacios <jordipalacioseso@gmail.com>
2024-10-07 20:10:02 +0200

    nav menu improved with better labels

--- src/components/navbarMenu.jsx
+++ src/components/navbarMenu.jsx
@@ -1,27 +1,18 @@
-import React from 'react'
-import { NavItems } from './navItems'
+import React from "react";
+import { NavItems } from "./index";
 
 export const NavbarMenu = () => {
     return (
-        <div>
+        <header>
             <img src="https://via.placeholder.com/150" alt="logo" />
-            <NavItems 
-                href={"inicio"} 
-                item={"Inicio"}
-            />
-            <NavItems 
-                href={"guia-de-uso"} 
-                item={"Guia de Uso"}
-            />
-            <NavItems 
-                href={"reservar"} 
-                item={"Reservar"}
-            />
-            <NavItems 
-                href={"https://www.instagram.com/telefonoaudiolibro.es/"} 
-                item={"Eventos Reales"} 
+            <NavItems href={"inicio"} item={"Inicio"} />
+            <NavItems href={"guia-de-uso"} item={"Guia de Uso"} />
+            <NavItems href={"reservar"} item={"Reservar"} />
+            <NavItems
+                href={"https://www.instagram.com/telefonoaudiolibro.es/"}
+                item={"Eventos Reales"}
                 tBlank={true}
             />
-        </div>    
-    )
-}
+        </header>
+    );
+};
