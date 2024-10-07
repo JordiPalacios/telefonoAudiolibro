2e518efdb2ab8e3a7231a6b01bd697af69109862
JordiPalacios <jordipalacioseso@gmail.com>
2024-10-07 20:09:02 +0200

    nav menu and section created

--- src/Web.jsx
+++ src/Web.jsx
@@ -1,11 +1,15 @@
-import { Header } from "./components"
+import { Header, NavbarMenu, Section } from "./components/index"
 
 
 function Web() {
 
   return (
     <>
+      <NavbarMenu />
       <Header />   
+      <Section>
+
+      </Section>
     </>
   )
 }
