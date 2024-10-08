6efe1c98820b838fd285074e541d1f3b56d6ebd1
JordiPalacios <jordipalacioseso@gmail.com>
2024-10-08 20:17:21 +0200

    styles and workmode added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-export const Button = ({text}) => {
+export const Button = ({text, device}) => {
+    let buttonClasses = 'px-4 py-2 rounded-lg text-white font-semibold transition duration-300'
+
+    if (device === 'pc') {
+        buttonClasses += ' bg-blue-500 hover:bg-blue-700 active:bg-blue-800';
+    } else {
+        buttonClasses += ' bg-blue-500';
+    }
+    
     return (
-        <button>
+        <button className={buttonClasses}>
             {text}
         </button>
     )
