3b89a4c97faec4500288edbcf6828f63839eea02
JordiPalacios <jordipalacioseso@gmail.com>
2024-10-08 20:17:46 +0200

    added new component (backgroundImage) and working mode

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import { Button } from "./index";
+import { BackgroundImage, Button } from "./index";
+import { useWorkingMode } from "../assets/Customhooks/useWorkingMode";
 
 export const Header = () => {
+    const workingMode = useWorkingMode();
+
     return (
         <header>
-            <img
-                src="public/img/headerImg.png"
-                alt="Teléfono Audiolibro imagen portada"
-            />
-            <h1>Lo primero que se olvida de una persona es su voz</h1>
-            <h2>Escucha y guarda para siempre las voces de tus invitados</h2>
-            <Button text={"Reservar"} />
+            <BackgroundImage src={"public/img/headerImg.png"} alt={"Teléfono Audiolibro Evento"}>
+                <h1>Lo primero que se olvida de una persona es su voz</h1>
+                <h2>Escucha y guarda para siempre las voces de tus invitados</h2>
+                <Button text={"Reservar"} device={workingMode} />
+            </BackgroundImage>
         </header>
     );
 };
