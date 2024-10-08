db699eb68a7d04a823a7067e9638a2a42d9600a0
JordiPalacios <jordipalacioseso@gmail.com>
2024-10-08 20:18:05 +0200

    added switch for diferent styles for component

--- src/components/SectionContaier.jsx
+++ src/components/SectionContaier.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
-export const SectionContaier = ({children}) => {
+export const SectionContaier = ({children, device, id}) => {
+    let containerClasses = 'p-5 mx-auto'
+
+    switch (device) {
+        case 'mobile':
+            containerClasses += 'max-w-full'
+            break
+        case 'tablet':
+            containerClasses += 'max-w-screen-md'
+            break
+        case 'pc':
+            containerClasses += 'max-w-screen-lg'
+            break
+        default:
+            containerClasses += 'max-w-screen-lg'
+    }
     return (
-        <section>
+        <section id={id} className={containerClasses} >
             {children}
         </section>
     )
