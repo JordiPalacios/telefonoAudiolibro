4807f2349f7a15ece661518592a167a96d9378eb
JordiPalacios <jordipalacioseso@gmail.com>
2025-01-22 19:47:16 +0100

    mejora en el enrutamiento

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,19 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react()
-  ],
-})
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react'],
+          'react-dom': ['react-dom'],
+        },
+      },
+    },
+  },
+  server: {
+    historyApiFallback: true,
+  },
+});
+
