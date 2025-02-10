ab8b354ec090ed4bab35d72dba9628d4c7cf9c3f
JordiPalacios <jordipalacioseso@gmail.com>
2025-02-10 19:51:26 +0100

    borrador de la presentacion hecho

--- src/sections/homePage/Presentation.jsx
+++ src/sections/homePage/Presentation.jsx
@@ -3,21 +3,28 @@ import { NavbarMenu } from '../../components'
 
 export const Presentation = () => {
 
-    const handleClick = () => {
-        const element = document.getElementById('plataforma360')
-        if (element) {
-        element.scrollIntoView({ behavior: 'smooth', top: '0' })
-        }
-    }
-
 return (
-    <header>
+    <header className=' flex flex-row'>
         <NavbarMenu />
         <div className="presentationContainer">              
-            <h1>Audios que vas a querer conservar</h1>
-            <img src="public/img/telefonoaudiolibro/telf01.webp" alt="telefonoaudiolibro"/>
-            <h2>Un libro de visitas de audio vintage</h2>
-            <h2>Una nueva forma de revivir los mejores recuerdos de tus personas favoritas</h2>             
+            <h1>ESCUCHA Y GUARDA PARA
+                SIEMPRE LA VOZ DE TUS
+                INVITADOS
+            </h1>
+            <p>
+                Cada dia no se tiene a toda la familia y amigos juntos
+                Aprovecha para que todos te dejen sus mensajes
+            </p>
+            <button>
+                <a href="#contacto">¡Agenda tu reunión!</a>
+            </button>
+            <p>
+                El 99% de las personas que no lo tienen
+                en su boda se arrepienten. 
+            </p>
+        </div>
+        <div>
+            <img src="public/img/telefonoaudiolibro/telf01.webp" alt="telefonoaudiolibro"/>           
         </div>
     </header>
 )
