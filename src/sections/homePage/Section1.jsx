5627bbd6e9e451828cbd0cbf4d71dc82004663ef
JordiPalacios <jordipalacioseso@gmail.com>
2025-02-11 20:32:11 +0100

    problema del vídeo solucionado

--- src/sections/homePage/Section1.jsx
+++ src/sections/homePage/Section1.jsx
@@ -3,51 +3,45 @@ import "slick-carousel/slick/slick-theme.css";
 import TelefonoAudiolibroData from "../../assets/mocks/telefonoAudiolibroData.json";
 import { MediaSlider } from "../../components";
 import Slider from "react-slick";
+import { useEffect, useState } from "react";
 
 export const Section1 = () => {
 
-    // useEffect(() => {
-    //     const handleResize = () => {
-    //         const newSetttings =
-    //             window.innerWidth < 1024
-    //                 ? { ...sliderSettings, slidesToShow: 1 }
-    //                 : { ...sliderSettings, slidesToShow: 3 }
+    const telefonoAudiolibroData = TelefonoAudiolibroData
 
-    //         setSliderSettings(newSetttings)
-    //     }
+    useEffect(() => {
+        const handleResize = () => {
+            const newSetttings =
+                window.innerWidth < 1024
+                    ? { ...sliderSettings, slidesToShow: 1 }
+                    : { ...sliderSettings, slidesToShow: 3 }
 
-    //     window.addEventListener("resize", handleResize)
+            setSliderSettings(newSetttings)
+        }
 
-    //     handleResize()
+        window.addEventListener("resize", handleResize)
 
-    //     return () => {
-    //         window.removeEventListener("resize", handleResize)
-    //     }
-    // }, [])
+        handleResize()
 
-    // const [sliderSettings, setSliderSettings] = useState({
-    //     dots: false,
-    //     infinite: true,
-    //     speed: 500,
-    //     slidesToShow: 1,
-    //     slidesToScroll: 1,
-    // })
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, [])
 
-    const carouselData = TelefonoAudiolibroData
-
-    var sliderSettings ={
+    const [sliderSettings, setSliderSettings] = useState({
         dots: false,
         infinite: true,
         speed: 500,
-        slidesToShow: 3,
+        slidesToShow: 1,
         slidesToScroll: 1,
-    }
+    })
+
 
     return (
         <div>
         <h1 className='font-semibold text-emerald-300 my-1'>Carousel</h1>
         <Slider {...sliderSettings}>
-            {carouselData.map((data) => (
+            {telefonoAudiolibroData.map((data) => (
             <MediaSlider
             name={data.name}
             url={data.url}
